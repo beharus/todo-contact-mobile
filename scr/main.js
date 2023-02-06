ce56adad5fd4d7a20fba496cd70ec8df6621a40a
unknown <sd979141@gmail.com>
2023-02-06 21:27:51 +0900

    some

--- scr/main.js
+++ scr/main.js
@@ -1,27 +1,83 @@
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-AOS.init();
+// Add list
 
-AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-    
-  
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 400, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-  
+let ul = document.querySelector("ul");
+
+let colorAct = "";
+let save = document.querySelector(".save");
+let modal = document.querySelector(".modal");
+let name2 = document.querySelector(".name");
+let number = document.querySelector(".number");
+let color = document.querySelector(".color");
+let deleteElement = document.querySelector(".delete");
+let btn = document.querySelector(".plus");
+let bgcol = document.querySelector('.changecolor')
+let classSpan = `font-oswald text-2xl font-semibold`;
+console.log(classSpan);
+let classNum = "font-oswald opacity-[.8]";
+let bodyContent = document.querySelector('body')
+
+btn.addEventListener("click", (e) => {
+  modal.classList.remove("hidden");
+  modal.classList.add("block");
+
+  modal.style.display = "block";
+  console.log("clicked");
+});
+
+// SAVE
+save.addEventListener("click", () => {
+  if (number.value == "" && name2.value == "") {
+    alert("you don't write anything");
+  } else if (number.value == "") {
+    alert("you don't write your phone number");
+  } else if (name2.value == "") {
+    alert("you don't write your name");
+  } else {
+    let lenght1 = number.value;
+    let numlength = lenght1.length;
+    console.log(numlength);
+    if (numlength < 8) {
+      alert("your number is not long enought, \nat least 8 numbers please");
+    } else {
+      modal.classList.remove("block");
+      modal.classList.add("hidden");
+      modal.style.display = "none";
+
+      let li = document.createElement("li");
+      li.setAttribute(
+        "class",
+        "flex justify-between items-center cursor-pointer border-b-2 px-4 py-4"
+      );
+      let colorAct = color.value;
+      li.innerHTML = `<span class="${classSpan}" style="color: ${colorAct}">${name2.value}</span><span class="${classNum}" style="color: ${colorAct}"> ${number.value}</span/>`;
+      ul.appendChild(li);
+    }
+  }
+});
+
+// changecolor
+bgcol.addEventListener('click', () => {
+  let col = prompt('write the name of color or the fotmat of color(rgb, ...)')
+  bodyContent.style.background=`${col}`
+})
+
+
+// delete
+deleteElement.addEventListener("click", (e) => {
+  deleteElement.classList.toggle("bga");
+  ul.addEventListener("click", (e) => {
+    if (e.target.nodeName == "LI") {
+      e.target.remove();
+    }
   });
+});
+
+// Watch
+let span = document.getElementById("span");
+function time() {
+  let d = new Date();
+  let m = d.getMinutes();
+  let h = d.getHours();
+  span.textContent = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2);
+}
+setInterval(time, 1000);
